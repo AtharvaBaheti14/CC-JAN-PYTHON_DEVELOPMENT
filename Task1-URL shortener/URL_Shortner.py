e5d071f9a73c03b2c37e76be5c995f31999b7705
AtharvaBaheti14 <atharvabaheti14@gmail.com>
2023-01-27 16:46:05 +0530

    CodeClause Tasks

---
+++ Task1-URL shortener/URL_Shortner.py
@@ -0,0 +1,36 @@
+import requests
+
+def shorten_link(full_link, link_name):
+    API_KEY = 'e1f68b86686d2f4f09c04d80d1db0436'
+    BASE_URL = 'https://cutt.ly/api/api.php'
+
+    payload = {'key': API_KEY, 'short': full_link, 'name': link_name}
+    request = requests.get(BASE_URL, params=payload)
+    data = request.json()
+
+    print('')
+
+# shorten_link('https://www.google.com/search?q=apples', 'appdsjrgfius')
+
+    try:
+        title = data['url']['title']
+        short_link = data['url']['shortLink']
+
+        print('Title:', title)
+        print('Link:', short_link)
+    except:
+        status = data['url']['status']
+        print('Error Status:', status)
+
+
+link = input('Enter a Link ---> ')
+name = input('Give your link name ---> ')
+
+shorten_link(link, name)
+
+
+
+
+
+
+
